6c91ddf649a8a2998a78f577b0d5e83e9e4af5ab
AjayGitHubCraft <145157684+ajaysharma0@users.noreply.github.com>
2024-04-17 13:03:52 +0530

    Create Swap two numbers without third variable.c

---
+++ Basic C Programs on Control Statements/Swap two numbers without third variable.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    
+    scanf("%d %d",&a,&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("%d \n%d",a,b);
+    
+}
+//Swap two numbers without third variable
+
+/*
+a=10
+b=20
+
+*/
